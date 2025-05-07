69993be2ba8b6b419aaa5e6599359195d6a8ac16
LuciaDG13 <ldufondgonz@ensc.fr>
2025-05-07 12:18:34 +0200

    amélioration de réponse

--- PHP/Nouveau/reponse.php
+++ PHP/Nouveau/reponse.php
@@ -1,4 +1,20 @@
 <?php
+//variables de connexion
+$host = 'localhost';
+$dbname = 'gestion_de_notes_v2'; //Changer avec le nom de notre base de données
+$username = 'root'; //$username = 'root';
+$password = ''; //$password = '';
+try {
+    $bdd = new PDO('mysql:host='. $host .';dbname='. $dbname .';charset=utf8',
+    $username, $password);
+    echo 'connexion établie <br>' ; //vérifie la connexion
+} catch(Exception $e) {
+    // Si erreur, tout arrêter
+    die('Erreur : '. $e->getMessage());
+}
+
+
+
 if (isset($_GET['prenom']) && isset($_GET['nom'])) {
     $prenom = htmlspecialchars($_GET['prenom']);
     $nom = $_GET['nom'];
@@ -7,7 +23,7 @@ if (isset($_GET['prenom']) && isset($_GET['nom'])) {
     // On a trois étudiants
     if (($prenom == "Alban") &&($nom == "Campioni"))
     {
-        echo "Salut Alban!";
+        echo "Salut Alban !";
     }
 
     else if (($prenom == "Lucas") &&($nom == "Hermouet"))
@@ -17,16 +33,26 @@ if (isset($_GET['prenom']) && isset($_GET['nom'])) {
 
     else if (($prenom == "Lucia") &&($nom == "Dufond"))
     {
-        echo "Salut Lucia!";
+        echo "Salut Lucia !";
     }
 
     else
     {
         echo "L'identifiant ne correspond pas.";
     }
+
+    $bddHelper = new BDDHelper(Host: 'localhost', DbName: 'ProjetWebS6', Username: 'root', Password: '');
+    
+
+    $requete = 'SELECT Id FROM `eleves` WHERE Prenom LIKE ' . $prenom . ' AND Nom LIKE ' . $nom . ';';
+    $resultat = $bdd->query($requete);
+    $ligne = $resultat->fetch();  // On récupère la première ligne
+    echo $ligne;
     
 
 } else {
     echo "Erreur : données manquantes.";
 }
+
+
 ?>
