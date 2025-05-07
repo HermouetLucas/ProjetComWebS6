c943f60005c5d738e3b195457f425695b03e884b
LuciaDG13 <ldufondgonz@ensc.fr>
2025-05-07 11:52:03 +0200

    amélioration du get

--- PHP/Nouveau/reponse.php
+++ PHP/Nouveau/reponse.php
@@ -3,8 +3,29 @@ if (isset($_GET['prenom']) && isset($_GET['nom'])) {
     $prenom = htmlspecialchars($_GET['prenom']);
     $nom = $_GET['nom'];
 
+    // Au lieu d'utiliser une bdd, on met directement dans le code les identifiants
+    // On a trois étudiants
+    if (($prenom == "Alban") &&($nom == "Campioni"))
+    {
+        echo "Salut Alban!";
+    }
 
-    echo "Bonjour $prenom $nom";
+    else if (($prenom == "Lucas") &&($nom == "Hermouet"))
+    {
+        echo "Salut Lucas!";
+    }
+
+    else if (($prenom == "Lucia") &&($nom == "Dufond"))
+    {
+        echo "Salut Lucia!";
+    }
+
+    else
+    {
+        echo "L'identifiant ne correspond pas.";
+    }
+    
+
 } else {
     echo "Erreur : données manquantes.";
 }
