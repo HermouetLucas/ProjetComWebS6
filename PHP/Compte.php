080cfdfa8f6e4d25ef4c55885d3c7b3239f370bc
HermouetLucas <lucas.hermouet22@gmail.com>
2025-05-07 09:48:59 +0200

    bug fix compte et bdd php

--- PHP/Compte.php
+++ PHP/Compte.php
@@ -1,24 +1,23 @@
 <?php
 class Compte
 {
-    private $id;
     private $identifiant;
     private $mdp;
 
     private $bdd;
 
-    function __construct($Id, $Identifiant, $Mdp)
+    function __construct()
     {
-        $this->id = $Id;
-        $this->identifiant = $Identifiant;
-        $this->mdp = $Mdp;
+        
         $bddHelper = new BDDHelper(Host: 'localhost', DbName: 'ProjetWebS6', Username: 'root', Password: '');
         $this->bdd = $bddHelper->getBdd();
 
     }
 
-    public function Connect()
+    public function Connect($Identifiant, $Mdp)
     {
+        $this->identifiant = $Identifiant;
+        $this->mdp = $Mdp;
         $requete = 'SELECT Id FROM `Compte` WHERE Identifiant LIKE ' . $this->identifiant . ' AND Mdp LIKE ' . $this->mdp . ';';
         $resultat = $this->bdd->query($requete);
         $tab = $resultat->fetchall(PDO::FETCH_ASSOC);
