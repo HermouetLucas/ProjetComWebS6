f95b4080552dff6486d1019dcdc22027984efb4c
HermouetLucas <lucas.hermouet22@gmail.com>
2025-05-14 11:56:55 +0200

    Début notes

--- React/projetComWeb/src/App.jsx
+++ React/projetComWeb/src/App.jsx
@@ -4,28 +4,60 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function Formulaire(props) {
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const Click = () => {
+    props.action(nom, prenom);
+  }
+
   return (
     <>
       <div className="login-container">
         <h2 className="login-title">Connexion Étudiant</h2>
-        <form className="login-form" action="reponse.php" method="get">
-          <label for="prenom">Entrez votre prénom : </label>
-          <input type="text" id="prenom" name="prenom" required /><br /><br />
+        <form className="login-form">
+          <label>Entrez votre prénom : </label>
+          <input type="text" onChange={(e) => setPrenom(e.target.value)} required /><br /><br />
 
-          <label for="nom">Entrez votre nom : </label>
-          <input type="text" id="nom" name="nom" required /><br /><br /><br />
+          <label>Entrez votre nom : </label>
+          <input type="text" onChange={(e) => setNom(e.target.value)} required /><br /><br /><br />
 
-          <button type="submit">Envoyer</button>
+          <button type="submit" onClick={Click}>Envoyer</button>
         </form>
       </div>
     </>)
 }
 
+function Notes(props) {
+  return (
+    <>
+      <p>{props.libelle} : {props.note}</p>
+
+    </>)
+}
+
 function App() {
+  const [data, setData] = useState([{ Note: "", Libelle: "" }]);
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const ActionBoutonGet = (nom, prenom) => {
+
+    fetch(`localhost/ProjetS6/reponse.php/?prenom=${prenom}&nom=${nom}`)
+      .then(r => r.json())
+      .then(datas => {
+        setData(datas)
+        setNom(nom);
+        setPrenom(prenom);
+        console.log(nom)
+      })
+  }
 
   return (
     <>
-      <Formulaire />
+      <Formulaire action={ActionBoutonGet} />
+      <h1>Note de {nom} {prenom}</h1><br></br>
+      {data.map((item) => {
+        return <Notes key={item.Libelle} libelle={item.Libelle} note={item.Note} />
+      })}
     </>
   )
 }
