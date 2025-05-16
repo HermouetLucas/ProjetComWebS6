5922ad088c87b2efa4ff2db89a1492d329eba784
LuciaDG13 <ldufondgonz@ensc.fr>
2025-05-16 14:59:52 +0200

    changements dans le css

--- React/projetComWeb/src/App.jsx
+++ React/projetComWeb/src/App.jsx
@@ -6,20 +6,32 @@ import './App.css'
 function Formulaire(props) {
   const [nom, setNom] = useState("");
   const [prenom, setPrenom] = useState("");
+  //Changer le style une fois connecté
   const Click = () => {
     props.action(nom, prenom);
   }
 
+  // Le style pour la connexion change lorsqu'on cherche à se connecter
+  const container = props.showNotes ? "login-container-connected" : "login-container";
+  const title = props.showNotes ? "login-title-connected" : "login-title";
+  const login = props.showNotes ? "login-form-connected" : "login-form";
+  const form = props.showNotes ? "form-group-connected" : "form-group";
+
   return (
     <>
-      <div className="login-container">
-        <h2 className="login-title">Connexion Étudiant</h2>
-        <form className="login-form">
-          <label>Entrez votre prénom : </label>
-          <input type="text" onChange={(e) => setPrenom(e.target.value)} required /><br /><br />
+      <div className={container}>
+        <h2 className={title}>Connexion Étudiant</h2>
+        <form className={login}>
 
-          <label>Entrez votre nom : </label>
-          <input type="text" onChange={(e) => setNom(e.target.value)} required /><br /><br /><br />
+          <div className={form}>
+            <label>Entrez votre prénom : </label>
+            <input type="text" onChange={(e) => setPrenom(e.target.value)} required /><br /><br />
+          </div>
+
+          <div className="form-group">
+            <label>Entrez votre nom : </label>
+            <input type="text" onChange={(e) => setNom(e.target.value)} required /><br /><br /><br />
+          </div>
 
           <button type="button" onClick={Click} disabled={!nom || !prenom}>Envoyer</button>
         </form>
@@ -71,11 +83,13 @@ function App() {
       <Formulaire action={ActionBoutonGet} />
       {showNotes && (
         <>
-          <br></br>
-          <h1>Note de {prenom} {nom}</h1><br></br>
-          {data.map((item) => {
-            return <Notes key={item.Libelle} libelle={item.Libelle} note={item.Note} />
-          })}
+          <div className="notes-container">
+            <br></br>
+            <h1>Note de {prenom} {nom}</h1><br></br>
+            {data.map((item) => {
+              return <Notes key={item.Libelle} libelle={item.Libelle} note={item.Note} />
+            })}
+          </div>
         </>
       )
       }
